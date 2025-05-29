c20265abe90229e2fb4c4f6e58f934f72ebb79ac
ishanc2017 <ishanc2017@gmail.com>
2025-05-30 03:15:46 +0530

    Initial commit

---
+++ src/main/java/com/springboot/config_client/controller/ConfigClientController.java
@@ -0,0 +1,29 @@
+package com.springboot.config_client.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RefreshScope
+@RestController
+public class ConfigClientController {
+
+	@Value("${company}")
+	private String companyName;
+	
+	@Value("${name}")
+	private String myName;
+	
+	@GetMapping("/company")
+	//@RequestMapping(path = "/company", method = RequestMethod.GET)
+	public String getCompanyName() {
+		return companyName;
+	}
+	
+	@GetMapping("/myname")
+	public String getMyName() {
+		return myName;
+	}
+	
+}
